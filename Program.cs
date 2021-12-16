17ff62562112cfb875723e733364eadd517fc59a
LAPTOP-1M8RSRLB\Pai <tawan.th@mail.kmutt.ac.th>
2021-12-16 11:50:15 +0700

    ส่งข้อสอบข้อ9

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Final_test_programming._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input skill name : ");
+            string name = Console.ReadLine();
+            while (name != "?")
+            {
+                Node node = new Node(name);
+                Console.Write("Add dependency for {0} ? (Y/N) : ", name);
+                char Yes_No = char.Parse(Console.ReadLine());
+                if (Yes_No == 'Y' || Yes_No == 'y')
+                {
+                    Console.Write("Input skill name : ");
+                    name = Console.ReadLine();
+                    node.nextNode = node;
+                }
+                else if (Yes_No == 'N' || Yes_No == 'n')
+                {
+                    Console.WriteLine(node.name);
+                }
+                else 
+                    Console.WriteLine("Plese input Y/N.");          
+            }
+        }
+    }
+    class Node
+    {
+        public string name;
+        public Node nextNode;
+
+        public Node(string nameValue)
+        {    
+                name = nameValue;
+                nextNode = null;   
+        }
+    }
+}
