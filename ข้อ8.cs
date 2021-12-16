c71442170741c53852e138d5df408f2ba28e934f
LAPTOP-1M8RSRLB\Pai <tawan.th@mail.kmutt.ac.th>
2021-12-16 12:19:30 +0700

    ส่งข้อสอบข้อ8

---
+++ ข้อ8.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Final_test_programming._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input number : ");
+            int n  = int.Parse(Console.ReadLine());
+            char[] number = new char[n];
+
+            Console.WriteLine("Input char : ");
+            for(int i = 0;i < n; i++)
+            {
+                Console.Write("{0}.", i + 1);
+                number[i] = char.Parse(Console.ReadLine());
+            }
+            int minIndex;
+            for (int i = 0; i < n; i++)
+            {
+                minIndex = i;
+                for (int j = i; j < n; j++)
+                {
+                    minIndex = j;
+                }
+                Swap(ref number, i, minIndex);
+            }
+            Console.Write("Summation : ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("{0},", number[i]);
+            }
+        }
+        static void Swap(ref char[] number, int index1, int index2)
+        {
+            char tmp = number[index1];
+            number[index1] = number[index2];
+            number[index2] = tmp;
+        }
+    }
+}
